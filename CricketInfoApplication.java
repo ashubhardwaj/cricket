0393e3fe18edab194e7988741c29760dbfd342fe
ashubhardwaj <60948777+ashubhardwaj@users.noreply.github.com>
2025-03-29 13:16:44 +0530

    CricketInfoApplication.java

---
+++ CricketInfoApplication.java
@@ -0,0 +1,11 @@
+package com.example.cricketinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CricketInfoApplication.class, args);
+    }
+}
